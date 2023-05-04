1befdd9cc9705150d77d44949a256e17c7786eec
DominikAutischer <132549025+DominikAutischer@users.noreply.github.com>
2023-05-04 11:28:18 +0200

    Add files via upload

---
+++ Lab1-2_Autischer.py
@@ -0,0 +1,7 @@
+x = int(input('Geben Sie Zahl 1 ein: '))
+y = int(input('Geben Sie Zahl 2 ein: '))
+
+if y==0:
+    print("Division durch Null nicht möglich")
+else:
+    print("Das Ergebnis der Division ist",x/y)
