cebe27891eea35894ee22441d911e1a3ea6db92d
DominikAutischer <132549025+DominikAutischer@users.noreply.github.com>
2023-05-04 11:34:44 +0200

    Add files via upload

---
+++ Lab1-3_Autischer.py
@@ -0,0 +1,7 @@
+x = int(input('Geben Sie Zahl 1 ein: '))
+y = int(input('Geben Sie Zahl 2 ein: '))
+
+while y==0:
+    print("Division durch 0 nicht möglich")
+    y = int(input("Geben Sie Zahl 2 erneut ein"))
+    
